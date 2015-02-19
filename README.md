4a7d5aecfb3f1f808c6561666f13d256798fd10b
Dave Herman <dherman@mozilla.com>
2015-02-18 17:05:44 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Runzip
+
+a recursive unzip for node, based off of [yauzl](https://github.com/thejoshwolfe/yauzl)
+
+## Usage
+
+```js
+var unzip = require("runzip");
+var fs = require("fs");
+
+function isZip(entry) {
+  return /\.(zip|jar)$/.test(entry.fileName);
+}
+
+runzip.open("path/to/file.zip", { filter: isZip }, function(err, zipfile) {
+  if (err) throw err;
+  zipfile.on("entry", function(entry) {
+    if (/\/$/.test(entry.fileName)) {
+      // directory file names end with '/'
+      return;
+    }
+    zipfile.openReadStream(entry, function(err, readStream) {
+      if (err) throw err;
+      // entry.nestedPath is an array of the recursively nested zip filenames
+      var outputDir = entry.nestedPath.join("/");
+      // ensure the output directory exists
+      mkdirp(outputDir);
+      readStream.pipe(fs.createWriteStream(outputDir + "/" + entry.fileName));
+    });
+  });
+});
+```
+
+## API
+
+The API provided is the same as [yauzl](https://github.com/thejoshwolfe/yauzl), but with an additional `filter` option. The `filter` option should be a function that takes the zipfile entry object and produces a boolean indicating whether it is a nested zipfile. The default value is:
+
+```js
+function(entry) {
+  return /\.zip$/.test(entry.fileName);
+}
+```
+
+### open(path, [options], [callback])
+
+Just like [yauzl](https://github.com/thejoshwolfe/yauzl), but with an additional `filter` option.
+
+### fromFd(fd, [options], [callback])
+
+Just like [yauzl](https://github.com/thejoshwolfe/yauzl), but with an additional `filter` option.
+
+### fromBuffer(buffer, [options], [callback])
+
+Just like [yauzl](https://github.com/thejoshwolfe/yauzl), but with an additional argument that is an options object accepting a `filter` option.
+
+
+# License
+
+MIT
---
+++ runzip.js
@@ -0,0 +1,117 @@
+var util = require('util');
+var events = require('events');
+var yauzl = require('yauzl');
+var concatStream = require('concat-stream');
+var Immutable = require('immutable');
+
+util.inherits(Runzip, events.EventEmitter);
+
+function Runzip(options) {
+  this._filter = (options && options.filter) || function(entry) {
+    return /\.zip$/.test(entry.fileName);
+  };
+  this._liveZipfileCount = 0;
+}
+
+Runzip.prototype._unzip = function _unzip(zipfile) {
+  this._liveZipfileCount = 1;
+  this._unzipNested(zipfile, new Immutable.List());
+};
+
+Runzip.prototype._unzipNested = function _unzip(zipfile, nestedPath) {
+  var self = this;
+  zipfile.on('entry', function(entry) {
+    if (self._filter(entry)) {
+      self._liveZipfileCount++;
+      //console.log("^^ " + self._liveZipfileCount);
+      zipfile.openReadStream(entry, function(err, readStream) {
+        if (err) {
+          self.emit('error', err);
+          self.close();
+          return;
+        }
+        readStream.on('error', function(err) {
+          self.emit('error', err);
+          self.close();
+        });
+        readStream.pipe(concatStream(function(buffer) {
+          yauzl.fromBuffer(buffer, function(err, zipfile) {
+            if (err) {
+              self.emit('error', err);
+              self.close();
+              return;
+            }
+            // a wild recursion appears
+            self._unzipNested(zipfile, nestedPath.push(entry.fileName));
+          });
+        }));
+      });
+    } else {
+      entry.nestedPath = nestedPath.toArray();
+      self.emit('entry', entry);
+    }
+  });
+  zipfile.on('end', function() {
+    self._liveZipfileCount--;
+    //console.log("vv " + self._liveZipfileCount);
+    if (self._liveZipfileCount === 0) {
+      self.emit('end');
+    }
+  });
+};
+
+function makeZipfileHandler(options, callback) {
+  return function(err, zipfile) {
+    if (err) throw err;
+    var r = new Runzip(options);
+    r._unzip(zipfile);
+    callback(null, r);
+  };
+}
+
+function open(path, options, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = null;
+  }
+  yauzl.open(path, options, makeZipfileHandler(options, callback));
+}
+
+function fromFd(fd, options, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = null;
+  }
+  yauzl.fromFd(fd, options, makeZipfileHandler(options, callback));
+}
+
+function fromBuffer(buffer, options, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = null;
+  }
+  yauzl.fromBuffer(fd, makeZipfileHandler(options, callback));
+}
+
+exports.open = open;
+exports.fromFd = fromFd;
+exports.fromBuffer = fromBuffer;
+
+//yauzl.open('./tests/two-root-files.zip', function(err, zipfile) {
+//  console.log(zipfile.__proto__.__proto__.constructor);
+//});
+
+// function test(name) {
+//   return function(err, zipfile) {
+//     if (err) throw err;
+//     zipfile.on('entry', function(entry) {
+//       console.log("entry: " + name + " " + entry.nestedPath.join("::") + " " + entry.fileName);
+//     });
+//     zipfile.on('end', function() {
+//       console.log("end " + name);
+//     });
+//   };
+// }
+
+// open('./tests/two-root-files.zip', null, test("two-root-files"));
+// open('./tests/four-roots-and-a-zip.zip', null, test("four-roots-and-a-zip"));
